291948622772cca83aa5df60c81ad843aa8e4e78
disbeliever <nerevar101@mail.ru>
2011-10-17 21:33:18 +0400

    Rewritten OOP-style

--- serial.py
+++ serial.py
@@ -13,6 +13,155 @@ extensions = ["avi", "mkv", "mp4", "mpg"]
 CONFIG_FILE = os.path.expanduser("~/.serial.py.conf")
 
 
+class Serial():
+
+    def __init__(self, action, series=0):
+        self.config = ConfigParser.RawConfigParser()
+        self.cwd = os.getcwd()
+
+        if series is None and action == 'play':
+            self.series = self._get_series_from_db()
+            self._s_play_series(self.series)
+        elif action == 'play':
+            self.series = series
+            self._s_play_series(self.series)
+        elif action == 'play' and series == 0:
+            self._get_series_from_db()
+            self._s_play_series(self.series)
+        elif action == 'next':
+            self.series = self._get_series_from_db() + 1
+            self._s_play_series(self.series)
+            self._save_series_to_db()
+        elif action == 'set':
+            self.series = series
+            self._s_play_series(self.series)
+            self._save_series_to_db()
+
+    def action_play(self):
+        pass
+
+    def action_next(self):
+        pass
+
+    def _get_series_from_db(self):
+        self.config.read(CONFIG_FILE)
+
+        try:
+            series = self.config.getint('Main', self.cwd)
+        except ConfigParser.NoSectionError:
+            series = 1
+        return series
+
+    def _save_series_to_db(self):
+        self.config.read(CONFIG_FILE)
+        try:
+            self.config.add_section('Main')
+        except ConfigParser.DuplicateSectionError:
+            pass
+        self.config.set('Main', self.cwd, self.series)
+        with open(CONFIG_FILE, 'wb') as config_file:
+            self.config.write(config_file)
+
+    def _s_play(self, path, player="mplayer2"):
+        """Запускает mplayer для проигрывания файла"""
+        sp.call([player, path])
+
+    def _s_play_series(self, series):
+        # конструируем имя файла (включая полный путь)
+        path = os.path.join(self.cwd, construct_filename(series))
+        # запускаем плейер
+        try:
+            os.stat(path)
+            s_play(path)
+        except OSError, e:
+            #print "File not found: {0}".format(path)
+            print "Series {0} isn't here :(".format(series)
+            #return 1
+
+
+class Constructor():
+
+    def __init__(self, files, series=0):
+        self.files = files
+        self.series = series
+
+    def construct(self, series):
+        self.series = series
+        # 1st method
+        file_name = self._construct_filename_find_diff()
+        if file_name in self.files:
+            return file_name
+        # 2nd method
+        try:
+            file_name = self._construct_filename_dd_re()
+        except IndexError:
+            pass
+        except TypeError:
+            pass
+        if file_name:
+            return file_name
+
+    def _construct_filename_dd_re(self):
+        re_comp = re.compile(r'0*' + str(self.series) + r'\D+')
+        file_name = [re_comp.match(i) for i in self.files
+                     if (re_comp.match(i) is not None
+                         and i.split('.')[-1] in extensions)][0].group()
+        return file_name
+
+    def _construct_filename_find_diff(self):
+        """
+        Конструируем имя файла на основе поиска разницы.
+        Работает для случаев вроде:
+        Bla-bla - 01 tra-la-la.mkv
+        Bla-bla - 02 tra-la-la.mkv
+        """
+        # определяем имя файла для 1-й серии (это будет базовое имя файла)
+        path = self._find_file(r'.*0*{0}.*'.format(1))
+        # определяем позицию и длину подстроки, отвечающей за номер серии
+        (pos, length) = self._find_mask()
+        if not path:
+            return None
+        # конструируем имя файла для нужной нам серии
+        path = ''.join([path[0:pos],
+                        str(self.series).zfill(length),
+                        path[pos + length:]])
+        return path
+
+    def _find_file(self, regexp):
+        """Find file by given regexp"""
+        for i in self.files:
+            if re.match(regexp, i) and i.split('.')[-1] in extensions:
+                return i
+
+    def _find_diff(self, str1, str2):
+        pos = 0
+        length = 2
+        if len(str1) != len(str2):
+            print "These fucking filenames are uncomparable!"
+            return (0, 0)
+        for i in xrange(0, len(str1)):
+            if str1[i] != str2[i]:
+                if str1[i].isdigit:
+                    if pos == 0:
+                        pos = i - 1
+                    elif inner == True:
+                        length += 1
+            else:
+                continue
+        #print (pos, length)
+        return (pos, length)
+
+    def _find_mask(self):
+        files = [i for i in self.files if i.split('.')[-1] in extensions]
+        files.sort()
+
+        diff = find_diff(files[0], files[1])
+        #print diff
+        (pos, length) = (diff[0], diff[1])
+        #print files[0][pos:pos + length]
+        return (pos, length)
+
+
 def switch_bool(x):
     if x == False:
         res = True
@@ -48,15 +197,15 @@ def find_diff(str1, str2):
 def find_mask(path):
     files = [i for i in os.listdir(path) if i.split('.')[-1] in extensions]
     files.sort()
-    try:
-        diff = find_diff(files[0], files[1])
-        #print diff
-        (pos, length) = (diff[0], diff[1])
-        #print files[0][pos:pos + length]
-        return (pos, length)
-    except IndexError:
-        print "[error]: Something wrong with indices in find_mask()"
-        sys.exit(1)
+    #try:
+    diff = find_diff(files[0], files[1])
+    #print diff
+    (pos, length) = (diff[0], diff[1])
+    #print files[0][pos:pos + length]
+    return (pos, length)
+    #except IndexError:
+    #    print "[error]: Something wrong with indices in find_mask()"
+    #    sys.exit(1)
 
 
 def is_first_run():
@@ -79,7 +228,9 @@ def parse_args():
 def construct_filename_dd_re(series, files):
     re_comp = re.compile(r'0*' + str(series) + r'\D+')
 
-    path = [re_comp.match(i) for i in files if (re_comp.match(i) is not None and i.split('.')[-1] in extensions)][0].group()
+    path = [re_comp.match(i) for i in files
+            if (re_comp.match(i) is not None and
+                i.split('.')[-1] in extensions)][0].group()
     return path
 
 
@@ -112,7 +263,9 @@ def construct_filename(series):
     cwd = os.getcwd()
     # 1. First method
     try:
-        path = os.path.join(cwd, construct_filename_dd_re(int(series), os.listdir(cwd)))
+        path = os.path.join(cwd,
+                            construct_filename_dd_re(int(series),
+                                                     os.listdir(cwd)))
     except IndexError:
         pass
     except TypeError:
@@ -161,7 +314,7 @@ def get_series_from_db():
     try:
         series = config.getint('Main', os.getcwd())
     except ConfigParser.NoSectionError:
-        series = 1 
+        series = 1
     return series
 
 
@@ -189,31 +342,34 @@ def main():
         (opts, args) = getopt.getopt(sys.argv[1:], "h", ["help"])
     except:
         usage()
+
     if len(args) == 0:
-        series = get_series_from_db()
+        series = None
+        action = 'play'
     elif len(args) == 1:
         if args[0].isdigit():
+            action = 'play'
             series = args[0]
         elif args[0] == 'play':
-            series = get_series_from_db()
-            save_series_to_db(series)
+            #series = get_series_from_db()
+            #save_series_to_db(series)
+            action = 'play'
         elif args[0] == 'next':
-            series = get_series_from_db() + 1
-            save_series_to_db(series)
+            #series = get_series_from_db() + 1
+            #save_series_to_db(series)
+            series = None
+            action = 'next'
     elif len(args) == 2:
         if args[0] == 'set':
             series = args[1]
-            save_series_to_db(series)
+            #save_series_to_db(series)
+            action = 'set'
     else:
         print len(args)
         print "error"
         return 1
 
-    # if is_first_run():
-    #     s_play_first()
-    # else:
-    s_play_series(series)
-
+    serial = Serial(action, series)
 
 if __name__ == "__main__":
     sys.exit(main())
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import re
 import unittest
 
 from serial import find_diff, construct_filename_dd_re
+from serial import Constructor
 
 
 class MainTestCase(unittest.TestCase):
@@ -22,7 +23,8 @@ class MainTestCase(unittest.TestCase):
         self.assertIsNone(re.match(re_comp, "011. Первое знакомство.avi"))
 
         re_comp = r'0*' + str(2) + r'\D+'
-        self.assertIsNotNone(re.match(re_comp, "02. Тяжелое расставание.avi"))
+        self.assertIsNotNone(re.match(re_comp,
+                                      "02. Тяжелое расставание.avi"))
 
     def test_construct_filename_dd_re(self):
         files = ["01. Первое знакомство.avi", "02. Тяжелое расставание.avi"]
@@ -30,6 +32,43 @@ class MainTestCase(unittest.TestCase):
         self.assertEqual(path, "01. Первое знакомство.avi")
         path = construct_filename_dd_re(2, files)
         self.assertEqual(path, "02. Тяжелое расставание.avi")
+
+
+class ConstructorTestCase(unittest.TestCase):
+
+    def test_constructor_diff_normal(self):
+        files = ["[j22v] BLOOD-C - 01 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 02 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 03 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 04 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 05 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 06 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 07 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 08 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 09 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 10 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 11 (TBS 1280x720 x264).mp4",
+                 "[j22v] BLOOD-C - 12 (TBS 1280x720 x264).mp4"
+                 ]
+        constructor = Constructor(files)
+        (pos, length) = constructor._find_diff(files[0], files[1])
+        self.assertEqual(pos, 17)
+        self.assertEqual(length, 2)
+
+        self.assertEqual(constructor.construct(6),
+                         "[j22v] BLOOD-C - 06 (TBS 1280x720 x264).mp4")
+
+        files = ["Dantalian no Shoka - 01 (TX 1280x720 x264).mp4",
+                 "Dantalian no Shoka - 02 (TX 1280x720 x264).mp4"]
+        constructor = Constructor(files)
+        self.assertEqual(constructor.construct(2),
+                         "Dantalian no Shoka - 02 (TX 1280x720 x264).mp4")
+
+    def test_constructor_dd(self):
+        files = ["01. Первое знакомство.avi", "02. Тяжелое расставание.avi"]
+        constructor = Constructor(files)
+        self.assertEqual(constructor.construct(1), "01. Первое знакомство.avi")
+        self.assertEqual(constructor.construct(2), "02. Тяжелое расставание.avi")
 
 if __name__ == "__main__":
     unittest.main()
