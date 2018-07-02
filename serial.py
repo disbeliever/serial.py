921c2f9bb02c8bccb6dc719b5b19514e963af1a3
disbeliever <nerevar101@mail.ru>
2018-07-02 21:10:04 +0400

    Separate some config working stuff into ConfigHelper class.

--- serial.py
+++ serial.py
@@ -18,54 +18,67 @@ CONFIG_FILE = os.path.expanduser("~/.serial.py.conf")
 player = "mpv"
 
 
-class Serial():
-    constructor = None
+class ConfigHelper():
 
-    def __init__(self, action, episode=0):
+    def __init__(self):
         self.config = configparser.RawConfigParser()
-        self.cwd = os.getcwd()
-        self.files = [i for i in os.listdir(self.cwd)
-                      if i.split('.')[-1].lower() in EXTENSIONS]
-        self.files.sort()
-
-        if episode == 0 and action == 'play':
-            self.episode = self._get_episode_from_db()
-            if (self.episode == 0):
-                self.episode += 1
-            self._s_play_episode(self.episode)
-            self._save_episode_to_db()
-        elif action == 'play':
-            self.episode = episode
-            self._s_play_episode(self.episode)
-        elif action == 'next':
-            self.episode = self._get_episode_from_db() + 1
-            self._s_play_episode(self.episode)
-            self._save_episode_to_db()
-        elif action == 'set':
-            self.episode = episode
-            self._s_play_episode(self.episode)
-            self._save_episode_to_db()
-
-    def _get_episode_from_db(self):
         self.config.read(CONFIG_FILE)
 
+    def get_episode_from_db(self, show_path):
         try:
-            episode = self.config.getint('Main', self.cwd)
+            episode = self.config.getint('Main', show_path)
         except configparser.NoSectionError:
             episode = 0
         except configparser.NoOptionError:
             episode = 0
         return episode
 
-    def _save_episode_to_db(self):
-        self.config.read(CONFIG_FILE)
+    def save_episode_to_db(self, show_path, episode):
         try:
             self.config.add_section('Main')
         except configparser.DuplicateSectionError:
             pass
-        self.config.set('Main', self.cwd, self.episode)
+        self.config.set('Main', show_path, episode)
         with open(CONFIG_FILE, 'w') as config_file:
             self.config.write(config_file)
+
+    def get_player(self):
+        try:
+            player = self.config.get('General', 'player')
+        except configparser.NoSectionError:
+            player = "mplayer"
+        except configparser.KeyError:
+            player = "mplayer"
+        return player
+
+
+class Serial():
+    constructor = None
+
+    def __init__(self, action, episode=0):
+        self.config = ConfigHelper()
+        self.cwd = os.getcwd()
+        self.files = [i for i in os.listdir(self.cwd)
+                      if i.split('.')[-1].lower() in EXTENSIONS]
+        self.files.sort()
+
+        if episode == 0 and action == 'play':
+            self.episode = self.config.get_episode_from_db(self.cwd)
+            if (self.episode == 0):
+                self.episode += 1
+            self._s_play_episode(self.episode)
+            self.config.save_episode_to_db(self.cwd, self.episode)
+        elif action == 'play':
+            self.episode = episode
+            self._s_play_episode(self.episode)
+        elif action == 'next':
+            self.episode = self.config.get_episode_from_db(self.cwd) + 1
+            self._s_play_episode(self.episode)
+            self.config.save_episode_to_db(self.cwd, self.episode)
+        elif action == 'set':
+            self.episode = episode
+            self._s_play_episode(self.episode)
+            self.config.save_episode_to_db()
 
     def _s_play_episode(self, episode):
         """Plays the episode with given number"""
@@ -244,15 +257,9 @@ def main():
     except:
         usage()
 
-    config = configparser.RawConfigParser()
-    config.read(CONFIG_FILE)
+    configHelper = ConfigHelper()
     global player
-    try:
-        player = config.get('General', 'player')
-    except configparser.NoSectionError:
-        player = "mplayer"
-    except configparser.KeyError:
-        player = "mplayer"
+    player = configHelper.get_player()
 
     if len(args) == 0:
         episode = 0
